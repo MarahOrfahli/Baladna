9e226c6d990d07393d6367267bab42e677410dcc
Marah Orfahli <marahorfahli7@gmail.com>
2026-08-08 14:40:30 +0300

    installing and setup  axios,installing  routers

---
+++ src/services/api/EndpointsPuplic.js
@@ -0,0 +1,20 @@
+export const API_ENDPOINTS_PUPLIC = {
+  Areas: {
+    LIST: "/areas",
+    DETAILS: (id) => `/areas/${id}`,
+    ByParent: (id, parent_id) => `/areas/${id}?parent_id=${parent_id}`
+  },
+  Agencies: {
+    LIST: "/agencies",
+    DETAILS: (id) => `/agencies/${id}`,
+    ACTIVITY: (id, is_active) => `/agencies/${id}?active=${is_active}`
+  },
+  Categories: {
+    LIST: "/categories",
+    DETAILS: (id) => `/categories/${id}`,
+    ACTIVITY: (id, is_active) => `/categories/${id}?active=${is_active}`,
+    AGENCY: (id, agency_id) => `/categories/${id}?agency_id=${agency_id}`,
+    ACTIVITY_AGENCY: (id, agency_id, is_active) => `/categories/${id}?agency_id=${agency_id}?&active=${is_active}`
+  },
+  
+};
